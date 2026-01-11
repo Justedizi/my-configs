c78ec68a1cb876705bf278b1a52c79d431580156
“Justedizi” <krzysztof99c@gmail.com>
2026-01-11 08:25:39 +0100

    afa

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true, -- Enable transparency
+      styles = {
+        sidebars = "transparent", -- Make sidebars (like neo-tree) transparent
+        floats = "transparent", -- Make floating windows transparent
+      },
+    },
+  },
+}
